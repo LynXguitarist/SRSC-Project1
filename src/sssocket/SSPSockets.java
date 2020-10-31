86190fe8a4d70aed5bfbe5eb55331649b0bc3d7f
frederico_lopes <fmc.lopes@campus.fct.unl.pt>
2020-10-31 14:55:04 +0000

    preparation for the ssp payload

--- src/sssocket/SSPSockets.java
+++ src/sssocket/SSPSockets.java
@@ -7,16 +7,31 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.Mac;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class SSPSockets extends DatagramSocket {
 
 	private static final String CONFIG_PATH = "D:\\FCT\\SRSC\\PRATICA\\PROJECTOS\\PROJECTO1\\SRSC-Project1\\configs-examples\\Phase-1";
 
-	private String ciphersuite, mac1, mac2;
+	private String ciphersuite, mac1, mac2, algorithm;
 	private byte[] iv, session_key, mac1_key, mac2_key;
 	private int session_key_size, mac1_key_size, mac2_key_size;
+
+	private IvParameterSpec ivSpec;
+	private Key secretKey, hMacKey, hMacKey2;
+	private Cipher cipher;
+	private Mac hMac, hMac2;
 
 	// Receives as input the file with the crypto to use
 	public SSPSockets(String filename) throws SocketException {
@@ -28,16 +43,47 @@ public class SSPSockets extends DatagramSocket {
 	 * Sends and encrypts packets
 	 */
 	public void send(DatagramPacket packet) {
+		try {
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec);
+			byte[] outputBytes = cipher.doFinal(packet.getData());
+			// generate payload E (KS, [ Mp || MAC1Km1 (Mp) ] ) || MAC2Km2 (C)
+			/**
+			 * C = E (KS, [ Mp || MACKM (Mp) ] 
+			 * Ks: symmetric session key 
+			 * Km1: MAC key 
+			 * Km2: a MAC key for Fast control DoS mitigation
+			 */
 
-		this.send(packet);
+			DatagramPacket outpacket = new DatagramPacket(outputBytes, outputBytes.length);
+			this.send(outpacket);
+		} catch (IllegalBlockSizeException | InvalidAlgorithmParameterException | BadPaddingException
+				| InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Receives and decipher the packet
 	 */
 	public void receive(DatagramPacket packet) {
+		try {
+			cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+			byte[] inputBytes = cipher.doFinal(packet.getData());
+			// generate payload E (KS, [ Mp || MAC1Km1 (Mp) ] ) || MAC2Km2 (C)
+			/**
+			 * C = E (KS, [ Mp || MACKM (Mp) ] 
+			 * Ks: symmetric session key 
+			 * Km1: MAC key 
+			 * Km2: a MAC key for Fast control DoS mitigation
+			 */
 
-		this.receive(packet);
+			DatagramPacket inpacket = new DatagramPacket(inputBytes, inputBytes.length);
+			this.receive(inpacket);
+		} catch (IllegalBlockSizeException | InvalidAlgorithmParameterException | BadPaddingException
+				| InvalidKeyException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -47,7 +93,25 @@ public class SSPSockets extends DatagramSocket {
 	 */
 	private void init(String filename) {
 		processFile(filename);
-		IvParameterSpec ivSpec = new IvParameterSpec(this.iv);
+		try {
+			// IV, KEY, SESSION_KEY,CIPHER_SUITE
+			ivSpec = new IvParameterSpec(this.iv);
+			secretKey = new SecretKeySpec(this.session_key, this.algorithm);
+			cipher = Cipher.getInstance(this.ciphersuite);
+
+			// MAC1
+			hMac = Mac.getInstance(this.mac1);
+			hMacKey = new SecretKeySpec(secretKey.getEncoded(), this.mac1);
+
+			// MAC2
+			hMac2 = Mac.getInstance(this.mac2);
+			hMacKey2 = new SecretKeySpec(secretKey.getEncoded(), this.mac2);
+
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	/**
@@ -100,6 +164,9 @@ public class SSPSockets extends DatagramSocket {
 				// mac2Key
 				this.mac2_key = in.readLine().split(":")[1].getBytes(",");
 			}
+
+			// get algorithm
+			this.algorithm = this.ciphersuite.split("/")[0];
 			in.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
